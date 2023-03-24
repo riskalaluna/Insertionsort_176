1546c3d79ea38b1c52accfe1b40424b9bb2e90b2
riskalaluna <riskalaluna@gmail.com>
2023-03-24 22:34:35 +0700

    looping nilai k dimulai dari 0

--- Insertionsort.cpp/Insertionsort.cpp.cpp
+++ Insertionsort.cpp/Insertionsort.cpp.cpp
@@ -41,3 +41,11 @@ void insertionsort() {                   //Procedure Insertionsort
             arr[j + 1] = arr[j];                     //4a. Simpan arr[j] ke dalam variable arr[j + 1]
             j--;                                     //4b. Decrement nilai j by 1
         }
+        arr[j + 1] = temp;                              //5. Simpan nilai temp ke dalam arr[j + 1]
+
+        cout << "\nPass " << i << ": ";         //Output ke layar
+        for (int k = 0; k < n; k++) {           //Looping nilai k dimulai dari 0 hingga n - 1
+            cout << arr[k] << "";               //Output ke layar
+        }
+    }
+}
