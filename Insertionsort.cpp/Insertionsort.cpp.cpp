25fb2946314c85ad6de1accacb741fca6b474f6d
riskalaluna <riskalaluna@gmail.com>
2023-03-24 22:26:35 +0700

    Looping dengan i dimulai dari 1 hingga n-1

--- Insertionsort.cpp/Insertionsort.cpp.cpp
+++ Insertionsort.cpp/Insertionsort.cpp.cpp
@@ -27,3 +27,14 @@ void input() {          //Procedure Input
     }
 }
 
+void insertionsort() {                   //Procedure Insertionsort
+
+    int temp;                            //Membuat variable data temporer atau penyimpanan sementara
+    int j;                               //Membuat variable j sebagai penanda
+
+    for (int i = 1; i < n; i++) {                     // 1. Looping dengan i dimulai dari 1 hingga n-1
+        temp = arr[i];                                // 2. Simpan nilai arr[i] ke variable sementara temp
+        j = i - 1;                                    // 3. Setting nilai j sama dengan i - 1
+        j = i - 1;                                      // 3. Setting nilai j sama dengan i - 1
+        while (j >= 0 && arr[j] > temp)                 // 4. Looping while dimana nilai j lebih besar sama dengan 0 dan
+            // ar[j] lebih besar daripada temp
