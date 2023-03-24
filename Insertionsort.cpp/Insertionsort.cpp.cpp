ebb2bde420082d7903dfcc090b452f701c633faa
riskalaluna <riskalaluna@gmail.com>
2023-03-24 22:16:35 +0700

    menggunakan perulangan for untuk menyimpan data

--- Insertionsort.cpp/Insertionsort.cpp.cpp
+++ Insertionsort.cpp/Insertionsort.cpp.cpp
@@ -21,3 +21,9 @@ void input() {          //Procedure Input
     cout << "Masukkan Element Array" << endl;
     cout << "======================" << endl;
 
+    for (int i = 0; i < n; i++) {                       //Menggunakan perulangan for untuk menyimpan data pada array
+        cout << "Data ke- " << (i + 1) << ": ";         //Memasukkan atau menginputkan nilai data n
+        cin >> arr[i];                                  //Menyimpan nilai data n kedalam array arr
+    }
+}
+
