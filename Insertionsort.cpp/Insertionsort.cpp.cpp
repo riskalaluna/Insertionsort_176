659b5db7819c336f171b085c1014eef4cbcd37bb
riskalaluna <riskalaluna@gmail.com>
2023-03-24 22:42:00 +0700

    looping dengan j dimulai dari 0

--- Insertionsort.cpp/Insertionsort.cpp.cpp
+++ Insertionsort.cpp/Insertionsort.cpp.cpp
@@ -55,3 +55,7 @@ void display() {                                            //Procedure display
     cout << "\n=================================" << endl;  //Output ke layar
     cout << "Element Array yang telah tersusun" << endl;    //Output ke layar
     cout << "==================================" << endl;   //Output ke layar
+
+    for (int j = 0; j < n; j++) {                               //Looping dengan j dimulai dari 0 hingga n - 1
+        cout << arr[j] << endl;                                 //Output ke layar
+    }
