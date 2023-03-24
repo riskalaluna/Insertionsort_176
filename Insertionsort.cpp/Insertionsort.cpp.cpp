dfff3b9607f7ef489eb2bd6fa8556d3d7c90872d
riskalaluna <riskalaluna@gmail.com>
2023-03-24 22:13:27 +0700

    membuat jarak per garis dan membuat tampilan susunan data elemen array

--- Insertionsort.cpp/Insertionsort.cpp.cpp
+++ Insertionsort.cpp/Insertionsort.cpp.cpp
@@ -16,3 +16,8 @@ void input() {          //Procedure Input
             cout << "\nArray yang anda masukkan maksimal 20 Elemen. \n";   //Menampilkan Pesan jika data lebih dari 20
         }
     }
+    cout << endl;                                      //Membuat jarak per baris program
+    cout << "======================" << endl;          //Membuat tampilan susunan data element array
+    cout << "Masukkan Element Array" << endl;
+    cout << "======================" << endl;
+
