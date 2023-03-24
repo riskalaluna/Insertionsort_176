d18d7b1723a8d173de1872fe389aa68f4110e66d
riskalaluna <riskalaluna@gmail.com>
2023-03-24 22:09:16 +0700

    b

--- Insertionsort.cpp/Insertionsort.cpp.cpp
+++ Insertionsort.cpp/Insertionsort.cpp.cpp
@@ -3,3 +3,16 @@ using namespace std;
 
 int arr[20];            //Membuat array dengan panjang data 20
 int n;                 //Membuat Variable inputan n
+
+void input() {          //Procedure Input
+    while (true) {
+        cout << "Masukkan Jumlah Data pada Array : ";     //Membuat Inputan jumlah element array
+        cin >> n;                                    //Memanggil variable inputan n
+
+        if (n <= 20) {                               //Membuat kondisi n tidak lebih dari sama den 20
+            break;
+        }
+        else {
+            cout << "\nArray yang anda masukkan maksimal 20 Elemen. \n";   //Menampilkan Pesan jika data lebih dari 20
+        }
+    }
